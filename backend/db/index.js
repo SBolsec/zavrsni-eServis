3cd701e970abdf7cbef73b76f3a6d69308e34dc2
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-03-07 23:15:28 +0100

    spajanje na bazu #4

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,12 @@
-const {Pool} = require('pg');
-const {dbConstants} = require('./dbConstants');
+const { Pool } = require('pg');
+const { dbConstants } = require('./dbConstants');
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = {
     query: (text, params) => {
