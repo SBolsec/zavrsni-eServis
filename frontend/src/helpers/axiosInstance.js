3f87e44c80e82f306c431ba002473aa4a4b28837
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-03-08 02:06:11 +0100

    fix backend link

--- frontend/src/helpers/axiosInstance.js
+++ frontend/src/helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import isAuthenticated from '../utils/isAuthenticated';
 
 const axiosWithHistory = (history = null) => {
-    const baseURL = 'http://eservis.herokuapp.com';
+    const baseURL = 'https://eservis.herokuapp.com';
 
     const axiosInstance = axios.create({
         baseURL: baseURL,
