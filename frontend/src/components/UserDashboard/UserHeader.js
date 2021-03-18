1132f0edb2b2ce8673b5e0a9e1c00e1cfa5a91f4
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2021-03-19 00:00:02 +0100

    promijenjen dashboard

--- frontend/src/components/UserDashboard/UserHeader.js
+++ frontend/src/components/UserDashboard/UserHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useUserContext } from '../../contexts/UserContext';
 import setShowSidebar from '../../actions/sidebar';
 import {
