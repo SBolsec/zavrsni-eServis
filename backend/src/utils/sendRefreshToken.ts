1e91c889f39364f80b610780a2d8892c2b8d5af8
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2021-03-29 11:15:18 +0200

    pokusaj popravka cookija #2

--- backend/src/utils/sendRefreshToken.ts
+++ backend/src/utils/sendRefreshToken.ts
@@ -1,5 +1,6 @@
 export const sendRefreshToken = (res: any, token: string) => {
   res.cookie("jid", token, {
+    domain: process.env.NODE_ENV == 'production' ? 'eservis.herokuapp.com' : 'localhost',
     secure: process.env.NODE_ENV == 'production' ? true : false,
     httpOnly: true,
     path: "/token"
