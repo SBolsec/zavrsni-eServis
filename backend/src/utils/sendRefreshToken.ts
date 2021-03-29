bdc417718af74a723d804d3ea07a286ec083de0a
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2021-03-29 11:08:07 +0200

    pokusaj popravka cookija

--- backend/src/utils/sendRefreshToken.ts
+++ backend/src/utils/sendRefreshToken.ts
@@ -1,5 +1,6 @@
 export const sendRefreshToken = (res: any, token: string) => {
   res.cookie("jid", token, {
+    secure: process.env.NODE_ENV == 'production' ? true : false,
     httpOnly: true,
     path: "/token"
   });
