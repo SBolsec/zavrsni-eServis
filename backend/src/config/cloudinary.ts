b9dba9271e97fa002ae1cd2580f6d98439ba5804
Siniša Bolšec <sinisa.bolsec@fer.hr>
2021-04-29 22:21:59 +0200

    environment variables fix types

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -2,8 +2,8 @@ import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
     cloud_name: 'eservis',
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY!,
+    api_secret: process.env.CLOUDINARY_SECRET!
 });
 
 export default cloudinary;
